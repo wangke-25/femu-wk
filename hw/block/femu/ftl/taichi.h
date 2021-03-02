2ef288f7f98d13b8e50282dddb09c5193909b056
wangke <296574397@qq.com>
2021-03-02 16:04:16 +0800

    taichi-1.1

--- hw/block/femu/ftl/taichi.h
+++ hw/block/femu/ftl/taichi.h
@@ -1,11 +1,11 @@
 #ifndef __DFTL_H
 #define __DFTL_H
 
-#define M 32
-#define N 64
+#define M 64
+#define N 32
 #define ENTRY_PER_PAGE 2048             // M*N
-#define MAX_MAPPING_CAHCHE_SIZE (112 * ENTRY_PER_PAGE)
-#define DFTL_HASH_MAP_SIZE (256 * ENTRY_PER_PAGE)         //1024*2
+#define MAX_MAPPING_CAHCHE_SIZE (60 * ENTRY_PER_PAGE)   //28
+#define DFTL_HASH_MAP_SIZE (128 * ENTRY_PER_PAGE)         //1024*2
 #define DFTL_NODE_ADD_SIZE 1
 
 #define WRITE 0
